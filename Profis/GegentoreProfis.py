508b5320f1591b329f9b934e9bbd35f249de36ba
Demmelbauer Mario <mario.demmelbauer@coc-ag.de>
2026-02-15 08:57:35 +0100

    Aktualisierung Daten

--- Profis/GegentoreProfis.py
+++ Profis/GegentoreProfis.py
@@ -70,7 +70,7 @@ def draw_field():
     # Legende seitlich links unten platzieren
     ax.legend(loc="lower left", fontsize=10)
 
-    plt.title("Profis 25/26 - Gegentore\n 2 Elfmeter u. 0 dir.FS n.b.")
+    plt.title("Profis 25/26 - Gegentore\n 3 Elfmeter u. 0 dir.FS n.b.")
     plt.xlabel("Spielfeldbreite (m)")
     plt.ylabel("Spielfeldtiefe (m)")
     return ax
